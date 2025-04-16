18949dc8fbfd461a71a9a3f63bc8d916302441ae
HZamzami <107877308+HZamzami@users.noreply.github.com>
2025-04-16 21:47:34 +0300

    feat: added age & medical history

--- src/types/medicalHistory.ts
+++ src/types/medicalHistory.ts
@@ -1,7 +1,7 @@
 export type MedicalHistoryType = {
-    mhid: String,
-    patient_id: String,
-    condition: String,
-    major_disease: String,
-    treatment: String
-}
+  mhid: String;
+  patient_id: String;
+  condition: String;
+  major_disease: String;
+  treatment: String;
+};
